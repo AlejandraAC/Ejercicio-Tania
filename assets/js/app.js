3500fae66c917c227cbb0e61f492618757172582
Ale Alvarado <ale.alvarado.contreras@gmail.com>
2024-06-28 20:43:20 -0600

    add: Se modificaron los archivos html y js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,28 @@
-const baseEndpoint = 'https://api.github.com';
-const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
-const $l = document.querySelector('.location');
+const baseEndpoint = 'https://api.github.com'; /* Enruta al URL de la API en github */
+const usersEndpoint = `${baseEndpoint}/users`; /* Enruta al URL de la API con los datos de usuario */
+const $n = document.querySelector('.name'); /* Selecciona un elemento del html con la clase "name". Le agregamos el punto para referenciar la clase  */
+const $b = document.querySelector('.blog'); /* Cambiamos el # por punto para referenciar una clase */
+const $l = document.querySelector('.location'); /* Agregamos la clase location en el documento HTML */
 
-function displayUser(username) {
-  $n.textContent = 'cargando...';
-  const response = await fetch(`${usersEndpoint}/${username}`);
-  console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+async function displayUser(username) { /* Agregamos la palabra async para la función asíncrona */
+  /* Agregamos los bloques de try y catch para el manejo de excepciones */
+  try{
+  $n.textContent = 'cargando...'; 
+  const response = await fetch(`${usersEndpoint}/${username}`); /* fetch hace la solicitud HTTP GET para traer lo que hay en usersEndPoint y username, await espera la respuesta */
+  const data = await response.json(); /* Convierte el cuerpo de response en formato JSON y lo guarda en data */
+  console.log(data); /* imprime data en consola */
+  $n.textContent = data.name; /* Quitamos las template literals de las tres variables, para que se muestren los valores y no las cadenas */
+  $b.textContent = data.blog;
+  $l.textContent = data.location;
+  } catch (err){
+    handleError(err);
+  }
 }
 
 function handleError(err) {
   console.log('OH NO!');
-  console.log(err);
+  console.error; /* le cambiamos console.log a console.error */
   n.textContent = `Algo salió mal: ${err}`
 }
 
-displayUser('stolinski').catch(handleError);
+displayUser('stolinski') /* Se quitó ".catch(handleError); y se agregó el bloque try y catch en la función asíncrona" */
